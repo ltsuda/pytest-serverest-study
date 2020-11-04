df88abb3ccb49e1a706d5c980d82b05aa01e6e3b
ltsuda <leonardo.tsuda@icloud.com>
2020-11-03 22:04:26 -0300

    add buscar carrinhos por query tests

--- tests/carrinhos/test_carrinhos.py
+++ tests/carrinhos/test_carrinhos.py
@@ -1,5 +1,5 @@
 from model.carrinho import ProdutoCarrinho, Carrinho
-
+import random
 import pytest
 import requests
 
@@ -14,17 +14,19 @@ class TestCarrinhos:
         auth_token = get_auth_token(usuario['email'], usuario['password'])
 
         produto = cadastrar_produto()
-        produto_carrinho = [ProdutoCarrinho(produto["_id"], 5)]
+        quantidade_produto = random.randint(1, 10)
+        produto_carrinho = [ProdutoCarrinho(
+            produto["_id"], quantidade_produto)]
         carrinho = cadastrar_carrinho(produto_carrinho, auth_token)
 
         carrinho_esperado = {
             "produtos": [{
                 "idProduto": produto["_id"],
-                "quantidade": 5,
+                "quantidade": quantidade_produto,
                 "precoUnitario": produto["preco"]
             }],
-            "precoTotal": produto["preco"] * 5,
-            "quantidadeTotal": 5,
+            "precoTotal": produto["preco"] * quantidade_produto,
+            "quantidadeTotal": quantidade_produto,
             "idUsuario": usuario["_id"],
             "_id": carrinho["_id"]
         }
@@ -35,3 +37,148 @@ class TestCarrinhos:
         assert resposta.status_code == 200
         assert resposta_de_sucesso["quantidade"] >= 1
         assert carrinho_esperado in resposta_de_sucesso["carrinhos"]
+
+    def test_buscar_carrinho_por_id(self, get_auth_token, cadastrar_usuario, cadastrar_produto, cadastrar_carrinho, carrinhos_url):
+        usuario = cadastrar_usuario(administrador="false")
+        auth_token = get_auth_token(usuario['email'], usuario['password'])
+
+        produto = cadastrar_produto()
+        quantidade_produto = random.randint(1, 10)
+        produto_carrinho = [ProdutoCarrinho(
+            produto["_id"], quantidade_produto)]
+        carrinho = cadastrar_carrinho(produto_carrinho, auth_token)
+
+        carrinho_esperado = {
+            "produtos": [{
+                "idProduto": produto["_id"],
+                "quantidade": quantidade_produto,
+                "precoUnitario": produto["preco"]
+            }],
+            "precoTotal": produto["preco"] * quantidade_produto,
+            "quantidadeTotal": quantidade_produto,
+            "idUsuario": usuario["_id"],
+            "_id": carrinho["_id"]
+        }
+
+        query = f'?_id={carrinho["_id"]}'
+        resposta = requests.get(carrinhos_url + query)
+        resposta_de_sucesso = resposta.json()
+        assert resposta.status_code == 200
+        assert resposta_de_sucesso["quantidade"] == 1
+        assert carrinho_esperado == resposta_de_sucesso["carrinhos"][0]
+
+    def test_buscar_carrinho_por_preco_total(self, get_auth_token, cadastrar_usuario, cadastrar_produto, cadastrar_carrinho, carrinhos_url):
+        usuario = cadastrar_usuario(administrador="false")
+        auth_token = get_auth_token(usuario['email'], usuario['password'])
+
+        produto = cadastrar_produto()
+        quantidade_produto = random.randint(1, 10)
+        produto_carrinho = [ProdutoCarrinho(
+            produto["_id"], quantidade_produto)]
+        carrinho = cadastrar_carrinho(produto_carrinho, auth_token)
+
+        carrinho_esperado = {
+            "produtos": [{
+                "idProduto": produto["_id"],
+                "quantidade": quantidade_produto,
+                "precoUnitario": produto["preco"]
+            }],
+            "precoTotal": produto["preco"] * quantidade_produto,
+            "quantidadeTotal": quantidade_produto,
+            "idUsuario": usuario["_id"],
+            "_id": carrinho["_id"]
+        }
+
+        query = f'?precoTotal={carrinho_esperado["precoTotal"]}'
+        resposta = requests.get(carrinhos_url + query)
+        resposta_de_sucesso = resposta.json()
+        assert resposta.status_code == 200
+        assert resposta_de_sucesso["quantidade"] >= 1
+        assert carrinho_esperado in resposta_de_sucesso["carrinhos"]
+
+    def test_buscar_carrinho_por_preco_total(self, get_auth_token, cadastrar_usuario, cadastrar_produto, cadastrar_carrinho, carrinhos_url):
+        usuario = cadastrar_usuario(administrador="false")
+        auth_token = get_auth_token(usuario['email'], usuario['password'])
+
+        produto = cadastrar_produto()
+        quantidade_produto = random.randint(1, 10)
+        produto_carrinho = [ProdutoCarrinho(
+            produto["_id"], quantidade_produto)]
+        carrinho = cadastrar_carrinho(produto_carrinho, auth_token)
+
+        carrinho_esperado = {
+            "produtos": [{
+                "idProduto": produto["_id"],
+                "quantidade": quantidade_produto,
+                "precoUnitario": produto["preco"]
+            }],
+            "precoTotal": produto["preco"] * quantidade_produto,
+            "quantidadeTotal": quantidade_produto,
+            "idUsuario": usuario["_id"],
+            "_id": carrinho["_id"]
+        }
+
+        query = f'?quantidadeTotal={carrinho_esperado["quantidadeTotal"]}'
+        resposta = requests.get(carrinhos_url + query)
+        resposta_de_sucesso = resposta.json()
+        assert resposta.status_code == 200
+        assert resposta_de_sucesso["quantidade"] >= 1
+        assert carrinho_esperado in resposta_de_sucesso["carrinhos"]
+
+    def test_buscar_carrinho_por_usuario_id(self, get_auth_token, cadastrar_usuario, cadastrar_produto, cadastrar_carrinho, carrinhos_url):
+        usuario = cadastrar_usuario(administrador="false")
+        auth_token = get_auth_token(usuario['email'], usuario['password'])
+
+        produto = cadastrar_produto()
+        quantidade_produto = random.randint(1, 10)
+        produto_carrinho = [ProdutoCarrinho(
+            produto["_id"], quantidade_produto)]
+        carrinho = cadastrar_carrinho(produto_carrinho, auth_token)
+
+        carrinho_esperado = {
+            "produtos": [{
+                "idProduto": produto["_id"],
+                "quantidade": quantidade_produto,
+                "precoUnitario": produto["preco"]
+            }],
+            "precoTotal": produto["preco"] * quantidade_produto,
+            "quantidadeTotal": quantidade_produto,
+            "idUsuario": usuario["_id"],
+            "_id": carrinho["_id"]
+        }
+
+        query = f'?idUsuario={carrinho_esperado["idUsuario"]}'
+        resposta = requests.get(carrinhos_url + query)
+        resposta_de_sucesso = resposta.json()
+        assert resposta.status_code == 200
+        assert resposta_de_sucesso["quantidade"] == 1
+        assert carrinho_esperado == resposta_de_sucesso["carrinhos"][0]
+
+    def test_buscar_carrinho_por_quantidade_e_preco_total(self, get_auth_token, cadastrar_usuario, cadastrar_produto, cadastrar_carrinho, carrinhos_url):
+        usuario = cadastrar_usuario(administrador="false")
+        auth_token = get_auth_token(usuario['email'], usuario['password'])
+
+        produto = cadastrar_produto()
+        quantidade_produto = random.randint(1, 10)
+        produto_carrinho = [ProdutoCarrinho(
+            produto["_id"], quantidade_produto)]
+        carrinho = cadastrar_carrinho(produto_carrinho, auth_token)
+
+        carrinho_esperado = {
+            "produtos": [{
+                "idProduto": produto["_id"],
+                "quantidade": quantidade_produto,
+                "precoUnitario": produto["preco"]
+            }],
+            "precoTotal": produto["preco"] * quantidade_produto,
+            "quantidadeTotal": quantidade_produto,
+            "idUsuario": usuario["_id"],
+            "_id": carrinho["_id"]
+        }
+
+        query = f'?quantidadeTotal={carrinho_esperado["quantidadeTotal"]}&precoTotal={carrinho_esperado["precoTotal"]}'
+        resposta = requests.get(carrinhos_url + query)
+        resposta_de_sucesso = resposta.json()
+        assert resposta.status_code == 200
+        assert resposta_de_sucesso["quantidade"] == 1
+        assert carrinho_esperado == resposta_de_sucesso["carrinhos"][0]
