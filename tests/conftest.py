ee781f40ee01315b03cf091df43d4fd3a671b106
ltsuda <leonardo.tsuda@icloud.com>
2020-11-05 21:34:17 -0300

    rename fixtures files and update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 
-from fixtures.login import get_auth_token
-from fixtures.usuario import cadastrar_usuario
-from fixtures.produto import cadastrar_produto
-from fixtures.carrinho import cadastrar_carrinho
-from fixtures.url_locator import get_base_url_locator, login_url,\
+from fixtures.login.login import get_auth_token
+from fixtures.usuario.usuario import cadastrar_usuario
+from fixtures.produto.produto import cadastrar_produto
+from fixtures.carrinho.carrinho import cadastrar_carrinho
+from fixtures.localizador_url.localizador_url import get_base_url_locator, login_url,\
     usuarios_url, produtos_url, carrinhos_url
