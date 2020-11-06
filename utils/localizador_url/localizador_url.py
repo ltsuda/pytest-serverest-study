3d49ca6e92bdac6bcb05ed48d66a21d7f80594e2
ltsuda <leonardo.tsuda@icloud.com>
2020-11-05 22:31:02 -0300

    add documentation to localizador url class and its methods

--- utils/localizador_url/localizador_url.py
+++ utils/localizador_url/localizador_url.py
@@ -1,20 +1,48 @@
-class LocalizadorURL(object):
-    """
-    Initiates the base API url and returns each endpoint
-    exising in the ServeRest API.
+class LocalizadorURL:
+    """Classe utilitária para representar um localizador de URL.
+
+    Atributos:
+        ip: Endereço IP do servidor ServeRest
+        port: Porta do servidor ServeRest
     """
 
     def __init__(self, ip, port):
+        """Inicializa os atributos necessários da classe
+
+        Args:
+            ip (str): Endereço IP do servidor ServeRest
+            port (str): Porta do servidor ServeRest
+        """
         self.base_url = f'http://{ip}:{port}'
 
     def get_url_login(self):
+        """Constrói a URL de login
+
+        Returns:
+            str: Endpoint de login
+        """
         return f'{self.base_url}/login'
 
     def get_url_usuarios(self):
+        """Constrói a URL de usuários
+
+        Returns:
+            str: Endpoint de usuários
+        """
         return f'{self.base_url}/usuarios'
 
     def get_url_produtos(self):
+        """Constrói a URL de produtos
+
+        Returns:
+            str: Endpoint de produtos
+        """
         return f'{self.base_url}/produtos'
 
     def get_url_carrinhos(self):
+        """Constrói a URL de carrinhos
+
+        Returns:
+            str: Endpoint de carrinhos
+        """
         return f'{self.base_url}/carrinhos'
