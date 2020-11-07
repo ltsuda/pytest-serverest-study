a1a530613e0c3bb0aeb338ca8df2fcff4f61effa
ltsuda <leonardo.tsuda@icloud.com>
2020-11-07 00:27:25 -0300

    create test file to hold usuarios test cases with API post method
    This commit creates a test file to hold only test that uses
    API POST method. Also, adds a setup method for these tests
    and use faker uuid4 methos instead of password method as
    the API might have problems with some characters.
    
    Signed-off-by: ltsuda <leonardo.tsuda@icloud.com>

---
+++ tests/usuarios/test_post_usuarios.py
@@ -0,0 +1,44 @@
+from model import Usuario
+
+import requests
+import random
+import pytest
+
+
+class TestPOSTUsuarios:
+    """
+    Suite de testes do endpoint /usuarios e método POST
+    """
+
+    @pytest.fixture(autouse=True)
+    def setup_usuario(self, request, cadastrar_usuario):
+        if 'cadastro_usuario_existente' in request.keywords:
+            self.usuario_existente = cadastrar_usuario()
+        else:
+            self.usuario = Usuario()
+
+    def test_cadastrar_usuario(self, url_usuarios):
+        resposta = requests.post(url_usuarios, json={
+            "nome": self.usuario.nome,
+            "email": self.usuario.email,
+            "password": self.usuario.password,
+            "administrador": self.usuario.administrador
+        })
+
+        resposta_de_sucesso = resposta.json()
+        assert resposta.status_code == 201
+        assert resposta_de_sucesso["message"] == "Cadastro realizado com sucesso"
+        assert "_id" in resposta_de_sucesso
+
+    @pytest.mark.cadastro_usuario_existente
+    def test_cadastrar_usuario_ja_existente(self, faker, url_usuarios):
+        resposta = requests.post(
+            url_usuarios, json={
+                "nome": faker.name(),
+                "email": self.usuario_existente["email"],
+                "password": faker.uuid4(),
+                "administrador": random.choice(["true", "false"])
+            })
+        resposta_de_sucesso = resposta.json()
+        assert resposta.status_code == 400
+        assert resposta_de_sucesso["message"] == "Este email já está sendo usado"
