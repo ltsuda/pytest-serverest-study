a0022f29e8fdd49cadfdf8c333bf1fe3c8049609
ltsuda <leonardo.tsuda@icloud.com>
2020-11-15 23:26:12 -0300

    move valid and invalid data schema tests to main login test file

--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -14,7 +14,7 @@ class TestLogin:
         else:
             self.usuario = cadastrar_usuario()
 
-    def test_login_com_administrador(self, url_login):
+    def test_login_com_administrador(self, url_login, valida_schema):
         resposta = requests.post(url_login, json={
             "email": self.usuario["email"],
             "password": self.usuario["password"]
@@ -22,12 +22,12 @@ class TestLogin:
 
         resposta_de_sucesso = resposta.json()
         assert resposta.status_code == 200
+        valida_schema(suite='login', data=resposta_de_sucesso,
+                      filename='post')
         assert resposta_de_sucesso["message"] == "Login realizado com sucesso"
-        assert "authorization" in resposta_de_sucesso
-        assert "Bearer" in resposta_de_sucesso["authorization"]
 
     @pytest.mark.usuario_comum
-    def test_login_com_usuario_comum(self, url_login):
+    def test_login_com_usuario_comum(self, url_login, valida_schema):
         resposta = requests.post(url_login, json={
             "email": self.usuario["email"],
             "password": self.usuario["password"]
@@ -35,11 +35,11 @@ class TestLogin:
 
         resposta_de_sucesso = resposta.json()
         assert resposta.status_code == 200
+        valida_schema(suite='login', data=resposta_de_sucesso,
+                      filename='post')
         assert resposta_de_sucesso["message"] == "Login realizado com sucesso"
-        assert "authorization" in resposta_de_sucesso
-        assert "Bearer" in resposta_de_sucesso["authorization"]
 
-    def test_login_com_email_invalido(self, url_login):
+    def test_login_com_email_invalido(self, url_login, valida_schema):
         resposta = requests.post(url_login, json={
             "email": "invalid@gmail.com",
             "password": self.usuario["password"]
@@ -47,9 +47,11 @@ class TestLogin:
 
         resposta_com_erro = resposta.json()
         assert resposta.status_code == 401
+        valida_schema(suite='login', data=resposta_com_erro,
+                      filename='post_dados_invalidos')
         assert resposta_com_erro["message"] == "Email e/ou senha inválidos"
 
-    def test_login_com_senha_invalida(self, url_login):
+    def test_login_com_senha_invalida(self, url_login, valida_schema):
         resposta = requests.post(url_login, json={
             "email": self.usuario["email"],
             "password": "invalid"
@@ -57,4 +59,6 @@ class TestLogin:
 
         resposta_com_erro = resposta.json()
         assert resposta.status_code == 401
+        valida_schema(suite='login', data=resposta_com_erro,
+                      filename='post_dados_invalidos')
         assert resposta_com_erro["message"] == "Email e/ou senha inválidos"
--- tests/login/test_schema.py
+++ tests/login/test_schema.py
@@ -23,24 +23,3 @@ class TestLoginSchema:
         assert resposta.status_code == 200
         valida_schema(suite='login', data=resposta.json(),
                       filename='post')
-
-    def test_login_schema_sem_dados(self, url_login, valida_schema):
-
-        resposta = requests.post(url_login, json={})
-
-        assert resposta.status_code == 400
-        valida_schema(suite='login', data=resposta.json(),
-                      filename='post_sem_dados')
-
-    def test_login_schema_email_senha_invalidos(self, faker, url_login, valida_schema):
-
-        user = {
-            "email": self.usuario["email"],
-            "password": faker.uuid4()
-        }
-
-        resposta = requests.post(url_login, json=user)
-
-        assert resposta.status_code == 401
-        valida_schema(suite='login', data=resposta.json(),
-                      filename='post_dados_invalidos')
