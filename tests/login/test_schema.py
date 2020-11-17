f3273031e1145c5f727e76ce1c12c2736837476f
ltsuda <leonardo.tsuda@icloud.com>
2020-11-16 22:37:47 -0300

    fix login test schema to validate required fields

--- tests/login/test_schema.py
+++ tests/login/test_schema.py
@@ -7,19 +7,9 @@ class TestLoginSchema:
     Suite de testes do schema do endpoint /login
     """
 
-    @pytest.fixture(autouse=True)
-    def setup_usuario(self, cadastrar_usuario):
-        self.usuario = cadastrar_usuario()
-
     def test_login_schema(self, url_login, valida_schema):
+        resposta = requests.post(url_login, json={})
 
-        user = {
-            "email": self.usuario["email"],
-            "password": self.usuario["password"]
-        }
-
-        resposta = requests.post(url_login, json=user)
-
-        assert resposta.status_code == 200
+        assert resposta.status_code == 400
         valida_schema(suite='login', data=resposta.json(),
-                      filename='post')
+                      filename='post_sem_dados')
